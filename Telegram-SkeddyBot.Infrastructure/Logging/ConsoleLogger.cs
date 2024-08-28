b642275633f861e622b85394aa22cb8255d2bc64
Ammar Hammad <amargamal832909@gmail.com>
2024-08-28 04:30:26 +0300

    Add project files.

---
+++ Telegram-SkeddyBot.Infrastructure/Logging/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram_SkeddyBot.Infrastructure.Logging
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
